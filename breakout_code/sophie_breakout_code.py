76919416024950a671d657b3e8ced4f7392de111
sophiejuco <83367902+sophiejuco@users.noreply.github.com>
2024-04-17 19:47:17 -0400

    Update sophie_breakout_code.py

--- breakout_code/sophie_breakout_code.py
+++ breakout_code/sophie_breakout_code.py
@@ -1,11 +1,8 @@
 import os
 from stable_baselines3.common.callbacks import BaseCallback
-from stable_baselines3.common import env_checker
 from stable_baselines3.common.vec_env import VecFrameStack
-from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.env_util import make_atari_env
 from stable_baselines3 import DQN
-import gymnasium as gym
 
 class TrainAndSave(BaseCallback):
     def __init__(self, check_freq, save_path, verbose=1):
@@ -17,17 +14,14 @@ class TrainAndSave(BaseCallback):
         self.output_number = 0
 
     def _init_callback(self):
-
         if self.save_path is not None:
             os.makedirs(self.save_path, exist_ok=True)
 
     def _on_step(self):
         self.output_number += 1
         if self.output_number % self.check_freq == 0:
-            model_path = os.path.join(self.save_path, f'best_model_{self.output_number}')
-            self.model.save(model_path)
             mean_reward = round(sum(self.episode_rewards) / max(1, len(self.episode_rewards)), 2)
-            print(f"Output number: {self.output_number}, Number of episodes: {self.num_episodes}, Average reward: {mean_reward}")
+            print(f"Output {self.output_number}: Average over {len(self.episode_rewards)} episodes - Reward: {mean_reward}")
             self.episode_rewards = []
             self.num_episodes = 0
         return True
@@ -40,7 +34,6 @@ CHECKPOINT_DIR = './train/'
 LOG_DIR = './logs/'
 window = 4
 env = make_atari_env("ALE/Breakout-v5", n_envs=1, monitor_dir=LOG_DIR)
-# Frame-stacking with 4 frames
 vec_env = VecFrameStack(env, n_stack=window)
 
 callback = TrainAndSave(check_freq=100000, save_path=CHECKPOINT_DIR)
